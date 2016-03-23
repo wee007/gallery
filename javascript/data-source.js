706ef856b97fa7dc2d39cdfeea3049610b576d2b
Eric Wee <wee007@gmail.com>
2016-03-24 01:01:30 +1100

    Created gallery

---
+++ javascript/data-source.js
@@ -0,0 +1,47 @@
+var items = [
+    {
+        "path": "persian.jpg",
+        "title" : "Persian",
+        "type"  : "Cat"
+    },
+    {
+        "path": "bombay.jpg",
+        "title" : "Bombay",
+        "type"  : "Cat"
+    },
+    {
+        "path": "thai.jpg",
+        "title" : "Thai",
+        "type"  : "Cat"
+    },
+    {
+        "path": "akita.jpg",
+        "title" : "Akita",
+        "type"  : "Dog"
+    },
+    {
+        "path": "chow-chow.jpg",
+        "title" : "Chow Chow",
+        "type"  : "Dog"
+    },
+    {
+        "path": "terrier.jpg",
+        "title" : "Terrier",
+        "type"  : "Dog"
+    },
+    {
+        "path": "lorikeet.jpg",
+        "title" : "Lorikeet",
+        "type"  : "Bird"
+    },
+    {
+        "path": "yucatan.jpg",
+        "title" : "Yucatan",
+        "type"  : "Bird"
+    },
+    {
+        "path": "finch.jpg",
+        "title" : "Finch",
+        "type"  : "Bird"
+    }
+]
---
+++ javascript/gallery.js
@@ -0,0 +1,97 @@
+var s = {
+  galleryContent:      $('#content'),
+  gallerySearchField:  $('.gallery-search_field'),
+  gallerySearchButton: $('.gallery-search_button'),
+  galleryViewToggle:   $('.gallery-view_toggle')
+}
+
+showGridView(items);
+setupViewToggle(items);
+setupSearch(items);
+
+function setupSearch(items) {
+  s.gallerySearchButton.on('click', function() {
+    var new_items = [];
+    $.each(items, function(index, value) {
+      var regex = new RegExp(s.gallerySearchField.val(), 'gi');
+      if (value["title"].match(regex)) {
+        new_items.push(value);
+      }
+    });
+
+    s.galleryContent.empty();
+    if (s.galleryViewToggle.hasClass('gallery-grid_view')) {
+      showGridView(new_items);
+    } else {
+      showListView(new_items);
+    }
+    setupViewToggle(new_items);
+  });
+}
+
+function setupViewToggle(items) {
+  s.galleryViewToggle.off('click')
+  s.galleryViewToggle.on('click', function() {
+    if (s.galleryViewToggle.hasClass('gallery-grid_view')) {
+      s.galleryViewToggle.attr('src','images/icons/list-toggle.jpg');
+      s.galleryViewToggle.switchClass('gallery-grid_view','gallery-list_view');
+      showListView(items);
+    } else {
+      s.galleryViewToggle.attr('src','images/icons/grid-toggle.jpg');
+      s.galleryViewToggle.switchClass('gallery-list_view','gallery-grid_view');
+      showGridView(items);
+    }
+  });
+}
+
+function showGridView(items) {
+  $.each(items, function(index, value) {
+    if ($('.gallery-tile').length == 0) {
+      s.galleryContent.html('\
+        <figure class="gallery-tile">\
+          <div class="gallery-tile_container">\
+            <div class="gallery-image_section">\
+              <img src="images/'+value["path"]+'" alt="'+value["title"]+'" title="'+value["title"]+'" class="gallery-grid-image">\
+            </div>\
+            <figcaption class="gallery-grid-title">'+value["title"]+'</figcaption>\
+          </div>\
+        </figure>');
+    } else {
+      $('.gallery-tile').last().after('\
+        <figure class="gallery-tile">\
+          <div class="gallery-tile_container">\
+            <div class="gallery-image_section">\
+              <img src="images/'+value["path"]+'" alt="'+value["title"]+'" title="'+value["title"]+'" class="gallery-grid-image">\
+            </div>\
+            <figcaption class="gallery-grid-title">'+value["title"]+'</figcaption>\
+          </div>\
+        </figure>');
+    }
+  });
+}
+
+function showListView(items) {
+  s.galleryContent.html('\
+    <header class="gallery-header">\
+      <h2 class="gallery-heading">Title</h2>\
+      <h2 class="gallery-heading">Type</h2>\
+    </header>');
+
+  $.each(items, function(index, value) {
+    if ($('.gallery-row').length == 0) {
+      $('.gallery-header').after('\
+        <figure class="gallery-row">\
+          <img src="images/'+value["path"]+'" alt="'+value["title"]+'" title="'+value["title"]+'" class="gallery-list-image">\
+          <figcaption class="gallery-list-title">'+value["title"]+'</figcaption>\
+          <figcaption class="gallery-list-type">'+value["type"]+'</figcaption>\
+        </figure>');
+    } else {
+      $('.gallery-row').last().after('\
+        <figure class="gallery-row">\
+          <img src="images/'+value["path"]+'" alt="'+value["title"]+'" title="'+value["title"]+'" class="gallery-list-image">\
+          <figcaption class="gallery-list-title">'+value["title"]+'</figcaption>\
+          <figcaption class="gallery-list-type">'+value["type"]+'</figcaption>\
+        </figure>');
+    }
+  });
+}
